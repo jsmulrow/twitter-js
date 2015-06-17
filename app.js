c8e2fa6dcd99c80b2d6c4b362c6f73993f608f25
Jack Mulrow <jackmulrow@Jacks-MacBook-Pro.local>
2015-06-17 14:37:21 -0400

    completed swig engine

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var express = require('express');
+var morgan = require('morgan');
+var swig = require('swig');
 var app = express();
 
-var morgan = require('morgan');
-
 app.use(morgan('dev'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({cache: false});
+
 
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render('index', {title: 'Hall of Fame', people: people} );
 });
 
 app.listen(3000, function(e) {
-
-	// var host = server.address().address;
-	// var port = server.address().port;
-
 	console.log('Server listening');
 });
+
